c3252745ac757d5886b64199f0b6c3e3c02ad649
Simone Stitz <simone.stitz@ost.ch>
2024-06-14 19:48:32 +0200

    issue clean up

--- snippets/bode.m
+++ snippets/bode.m
@@ -1,4 +1,5 @@
 s = tf('s');
 G = 1 + 0.1 *s;     % UTF des Systems
 bode(G)             % Bode-Plot des Systems
+margin(G)           % Bode-Plot mit Stabilitaets- und Phasenreserve
 bodemag(G)          % Amplitudengang des Systems
